bf5015f508c2f56a76cd04acce11fa6be3b42717
jbavari <josh@drifty.com>
2015-08-03 15:36:36 -0600

    Refactor(io-config): Add readConfigFile method to handle reading the io-config file from an appdirectory passed. Add in unit tests for io-config as well as a default config file to be used.

---
+++ spec/io-config.spec.js
@@ -0,0 +1,60 @@
+var archiver = require('archiver'),
+    events = require('../lib/events'),
+    fs = require('fs'),
+    helpers = require('./helpers'),
+    IoConfig = require('../lib/io-config'),
+    path = require('path'),
+    Project = require('../lib/project'),
+    ioConfigJson = require('./.io-config.json'),
+    Q = require('q');
+    // rewire = require('rewire'),
+    // settings = require('../lib/settings');
+
+var testDir = '/ionic/app',
+    configPath = __dirname;
+
+ddescribe('IoConfig', function() {
+
+  beforeEach(function() {
+  });
+
+  it('should have IoConfig defined', function() {
+    expect(IoConfig).toBeDefined();
+  });
+
+  it('should throw an exception when no app directory is passed', function() {
+    expect(IoConfig.warnMissingData).toThrow('You must specify the app directory of the config you wish to list.');
+  });
+
+  iit('should allow calls with app directories', function(done) {
+    var promise = Q.defer();
+    spyOn(Q, 'defer').andReturn(promise);
+    spyOn(fs, 'readFile').andCallFake(function(path, callBack) {
+      promise.resolve(true);
+    });
+
+    Q()
+    .then(function(){
+      // return IoConfig.warnMissingData(testDir);
+      return Q.resolve('hello');
+    })
+    .then(function(result) {
+      // expect(result).toBe(true);
+      console.log(result);
+      return Q.reject('blah');
+    })
+    .catch(function(ex){
+      expect('this').toBe(ex.stack);
+    })
+    .fin(done);
+  });
+
+  it('should read in the config file from an app directory', function() {
+    var json = IoConfig.readConfigFile(configPath);
+
+    expect(json).toBeDefined();
+    expect(json.app_id).toBe('some_app_id');
+    expect(json.api_key).toBe('some_api_key');
+
+  });
+});
